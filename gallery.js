df01b88391146c358f586921e909c2ba425dfea4
MarielMifsud1 <33513128+MarielMifsud1@users.noreply.github.com>
2018-04-21 14:33:45 +0200

    Commit 2
    1.Uploaded project proposal
    2.Upoaded milestone 2
    3. Made about us page
    4. Made portofolio page

---
+++ gallery.js
@@ -0,0 +1,26 @@
+function two() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.msFlex = "50%";  // IE10
+        elements[i].style.flex = "50%";
+    }
+}
+
+// for when the user scrolls 20px from the top of page
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topBtn").style.display = "block";
+    } else {
+        document.getElementById("topBtn").style.display = "none";
+    }
+}
+
+//when the button is clicked it goes to the top
+
+function goTop()
+{
+    
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
